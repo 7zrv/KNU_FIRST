5f4416e017b80afe2225fe9ac9295b2d0f4547f8
7zrv <4014133@gmail.com>
2023-07-09 20:18:39 +0900

    Initial commit

---
+++ src/main/java/com/example/knu_first/KnuFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.knu_first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnuFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnuFirstApplication.class, args);
+	}
+
+}
