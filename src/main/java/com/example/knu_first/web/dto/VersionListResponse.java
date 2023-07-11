de54a787406b161ad30bb21e5297485e4e376d1c
lhw93 <lhw9391@gmail.com>
2023-07-11 11:28:29 +0900

    Service, dto

---
+++ src/main/java/com/example/knu_first/web/dto/VersionListResponse.java
@@ -0,0 +1,26 @@
+package com.example.knu_first.web.dto;
+
+
+import com.example.knu_first.versionConfig.entity.VersionConfig;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class VersionListResponse {
+    private final int idx;
+    private final String os;
+    private final String ver;
+    private final boolean updatetype;
+    private final String message;
+    private final LocalDateTime regdate;
+
+    public VersionListResponse(VersionConfig versionConfig) {
+        this.idx = versionConfig.
+        this.os = os;
+        this.ver = ver;
+        this.updatetype = updatetype;
+        this.message = message;
+        this.regdate = regdate;
+    }
+}
