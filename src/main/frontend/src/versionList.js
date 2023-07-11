369781089fdec5d835a931bfe86851e0976a3830
sarumani <qkre2018@gmail.com>
2023-07-11 15:12:13 +0900

    versionList css 적용

--- src/main/frontend/src/versionList.js
+++ src/main/frontend/src/versionList.js
@@ -2,52 +2,102 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./VersionList.css";
 const VersionList = () => {
-  const tempData = (
-    <section className="record">
-      <tr key="1">
-        <td>1</td>
-        <td>iOS</td>
-        <td>1.0</td>
-        <td>true</td>
-        <td>null</td>
-        <td>com.test.myApp</td>
-        <td>2023-01-01</td>
-        <td>
-          <button className="testBtn">Test</button>
-          <button className="modBtn">수정</button>
-          <button className="delBtn">삭제</button>
-        </td>
-      </tr>
-    </section>
-  );
+  const onClickTestBtn = function () {
+    console.log("Test button clicked");
+  };
+  const onClickModBtn = function () {
+    console.log("Mod button clicked");
+  };
+  const onClickDelBtn = function () {
+    console.log("Del button clicked");
+  };
+
+  const dummy = [
+    {
+      idx: "1",
+      os: "iOS",
+      ver: "1.0",
+      updatetype: "true",
+      message: "null",
+      package: "com.test.myApp",
+      regdate: "2023-01-01",
+    },
+    {
+      idx: "2",
+      os: "iOS",
+      ver: "1.0",
+      updatetype: "true",
+      message: "null",
+      package: "com.test.myApp",
+      regdate: "2023-01-01",
+    },
+    {
+      idx: "3",
+      os: "iOS",
+      ver: "1.0",
+      updatetype: "true",
+      message: "null",
+      package: "com.test.myApp",
+      regdate: "2023-01-01",
+    },
+    {
+      idx: "4",
+      os: "iOS",
+      ver: "1.0",
+      updatetype: "true",
+      message: "null",
+      package: "com.test.myApp",
+      regdate: "2023-01-01",
+    },
+  ];
 
   const [list, setList] = useState([]);
+  // useEffect(() => {
+  //   axios
+  //     .get("/api/vercontrol/getConfigAll")
+  //     .then((res) => {
+  //       setList(res.data);
+  //     })
+  //     .catch((err) => {
+  //       return err;
+  //     });
+  // });
+
   useEffect(() => {
-    axios
-      .get("/api/vercontrol/getConfigAll")
-      .then((res) => {
-        setList(res.data);
-      })
-      .catch((err) => {
-        return err;
-      });
-  });
+    console.log("실행");
+    setList(dummy);
+  }, []);
+
   const tableList = list.map((item) => {
+    let bgColor = "red";
+    if (list.indexOf(item) % 2 === 1) {
+      bgColor = "cadetblue";
+    } else {
+      bgColor = "gainsboro";
+    }
     return (
-      <tr key={item.idx}>
-        <td>{item.idx}</td>
-        <td>{item.os}</td>
-        <td>{item.ver}</td>
-        <td>{item.updatetype}</td>
-        <td>{item.message}</td>
-        <td>{item.pacakge}</td>
-        <td>{item.regdate}</td>
-        <td>
-          <button>Test</button>
-          <button>수정</button>
-          <button>삭제</button>
-        </td>
-      </tr>
+      <section className="record" style={{ backgroundColor: bgColor }}>
+        <tr key={item.idx}>
+          <td>{item.idx}</td>
+          <td>{item.os}</td>
+          <td>{item.ver}</td>
+          <td>{item.updatetype}</td>
+          <td>{item.message}</td>
+          <td>{item.pacakge}</td>
+          <td>{item.regdate}</td>
+          <td>
+            <button className="testBtn" onClick={onClickTestBtn}>
+              Test
+            </button>
+            <button className="modBtn" onClick={onClickModBtn}>
+              수정
+            </button>
+            <button className="delBtn" onClick={onClickDelBtn}>
+              삭제
+            </button>
+          </td>
+        </tr>
+      </section>
     );
   });
   return (
@@ -64,7 +114,7 @@ const VersionList = () => {
             <th>regdate</th>
             <th>action</th>
           </tr>
-          {tempData}
+          {tableList}
         </thead>
       </table>
     </section>
