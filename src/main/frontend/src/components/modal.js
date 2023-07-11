a4d324633b21d3ac7687c2538089e3931a6927b0
sarumani <qkre2018@gmail.com>
2023-07-11 20:53:05 +0900

    테이블, 모달 팝업

--- src/main/frontend/src/components/modal.js
+++ src/main/frontend/src/components/modal.js
@@ -1,29 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import axios from "axios";
 
-const Modal = (props) => {
-  const { versionList, setOpenTest } = props;
-  const [selectValue, setSelectValue] = React.useState("");
-  const [result, setResult] = React.useState("");
+const MyModal = (props) => {
+  const { isModalOpen, toggleModal, versionList } = props;
+  const [selectValue, setSelectValue] = useState("");
+  const [result, setResult] = useState("");
+
   const selectList =
-    versionList ||
-    versionList.map((item) => {
-      return (
-        <option value={item.ver}>
-          {item.idx}-{item.os}-{item.ver}
-        </option>
-      );
-    });
-  const onClick = () => {
-    setOpenTest(false);
-  };
+    versionList &&
+    versionList.map((item) => (
+      <option value={item.ver} key={item.idx}>
+        {item.idx}-{item.os}-{item.ver}
+      </option>
+    ));
+
   const selectChange = (e) => {
     setSelectValue(e.target.value);
   };
-  const getVersion = (e) => {
+
+  const getVersion = () => {
     const os = selectValue.split("-")[1];
     axios
-      .get("localhost:8080/api/vercontrol/getConfig", {
+      .get("http://localhost:8080/api/vercontrol/getConfig", {
         params: {
           os: os,
         },
@@ -33,22 +32,25 @@ const Modal = (props) => {
       })
       .catch((err) => {
         console.log(err);
-        return err;
       });
   };
-  return (
-    <section class="modal" onChange={selectChange}>
-      <div>Cient ver</div>
 
+  const handleConfirm = () => {
+    alert("확인 버튼이 클릭되었습니다.");
+  };
+
+  return (
+    <Modal isOpen={isModalOpen} onRequestClose={toggleModal}>
+      <div>Client Ver</div>
       <form>
-        <select>{versionList}</select>
+        <select onChange={selectChange}>{selectList}</select>
         <input type="button" value="제출" onClick={getVersion} />
       </form>
-      <div>Sever Result</div>
-      <textarea value={result} />
-      <button onClick={onClick}>확인</button>
-    </section>
+      <div>Server Result</div>
+      <textarea value={result} readOnly />
+      <button onClick={handleConfirm}>확인</button>
+    </Modal>
   );
 };
 
-export default Modal;
+export default MyModal;
