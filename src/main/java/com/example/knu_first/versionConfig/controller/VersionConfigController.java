53e7446d081ed3f332b098f8771a6cd3e2939b37
7zrv <4014133@gmail.com>
2023-07-12 14:13:15 +0900

    versionConfig visible column추가 regdate 자동생성

--- src/main/java/com/example/knu_first/versionConfig/controller/VersionConfigController.java
+++ src/main/java/com/example/knu_first/versionConfig/controller/VersionConfigController.java
@@ -1,13 +1,12 @@
 package com.example.knu_first.versionConfig.controller;
 
 
-import com.example.knu_first.versionConfig.dto.AddVersionRequestDto;
 import com.example.knu_first.versionConfig.dto.OsRequestDto;
 import com.example.knu_first.versionConfig.dto.VersionConfigResponseDto;
+import com.example.knu_first.versionConfig.dto.VersionConfigUpdateRequestDto;
 import com.example.knu_first.versionConfig.entity.VersionConfig;
 import com.example.knu_first.versionConfig.service.VersionConfigService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,11 +19,7 @@ import java.util.stream.Collectors;
 public class VersionConfigController {
 
     private final VersionConfigService versionConfigService;
-    @PostMapping("/api/vercontrol/add")
-    public ResponseEntity<VersionConfig> addVersionConfigs(@RequestBody AddVersionRequestDto requestDto){
-        return  ResponseEntity.status(HttpStatus.CREATED)
-                .body(versionConfigService.save(requestDto));
-    }
+
     @GetMapping("/api/vercontrol/getConfigAll")
     public ResponseEntity<List<VersionConfigResponseDto>> findAllVersionConfigs(){
 
@@ -41,16 +36,25 @@ public class VersionConfigController {
         return ResponseEntity.ok().body(new VersionConfigResponseDto(versionConfigService.findVersionConfigByOs(osRequestDto.getOs())));
     }
 
-/*
-    @PutMapping("/api/vercontrol/update/{idx}")
-    public ResponseEntity<VersionConfig> updateV(@PathVariable Long idx,
-                                                       @RequestBody requestDto){
-        Article updatedArticle = blogService.update(id, requestDto);
 
-        return ResponseEntity.ok().body(updatedArticle);
+    @PutMapping("/api/vercontrol/update/{idx}")
+    public ResponseEntity<VersionConfig> updateVersionConfig(@PathVariable Long idx,
+                                                       @RequestBody VersionConfigUpdateRequestDto requestDto){
+
+        VersionConfig updatedVersionConfig = versionConfigService.updateVersionConfig(idx, requestDto);
+
+        return ResponseEntity.ok().body(updatedVersionConfig);
 
     }
-*/
+
+    @DeleteMapping("/api/vercontrol/delete/{idx}")
+    public String DeleteVersionConfig(@PathVariable Long idx){
+
+        versionConfigService.deleteVersionConfig(idx);
+        return "Delete Complete";
+
+    }
+
     @GetMapping("/api/makeDummyData")
     public String makeDummyData(){
         return versionConfigService.makeDumData();
