58f8805a727d0139434a619d0e920ab6457b6ce8
sarumani <qkre2018@gmail.com>
2023-07-13 14:41:34 +0900

    header 주석 처리

--- src/main/frontend/src/components/header.js
+++ src/main/frontend/src/components/header.js
@@ -1,22 +1,25 @@
-import { Component, useEffect, useState } from "react";
+import { useState } from "react";
 import "./header.css";
 import TestModal from "./testModal";
 import AddModal from "./addModal";
-import axios from "axios";
 
 export default function Header(props) {
   const { versionList, rendering } = props;
-  const [addModalOpen, setAddModalOpen] = useState(false);
-  const [testModalOpen, setTestModalOpen] = useState(false);
+  const [addModalOpen, setAddModalOpen] = useState(false); // 추가 모달의 열림 여부를 추적하는 상태 변수
+  const [testModalOpen, setTestModalOpen] = useState(false); // 테스트 모달의 열림 여부를 추적하는 상태 변수
+
   const onClickAdd = function () {
     console.log("Add Clicked");
-    setAddModalOpen(true);
+    setAddModalOpen(true); // 추가 모달 열기
   };
+
   const onClickTest = function () {
     console.log("Test Clicked");
-    setTestModalOpen(true);
+    setTestModalOpen(true); // 테스트 모달 열기
   };
+
   const closeModal = () => {
+    // 모달 닫기
     if (addModalOpen) {
       setAddModalOpen(!addModalOpen);
     }
